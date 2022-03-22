6eb24b6e2cf87a08c8d148d95e48091af62522a4
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-03-22 19:54:13 +0300

    adden icon and title favicon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
@@ -14,8 +16,8 @@ const Product = (props) => {
                 <p><small>Ratings: {ratings} Star</small></p>
             </div>
             <button onClick={() => addHandleCart(product)} className='btn-cart'>
-                <p>Add to Cart</p>
-
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
