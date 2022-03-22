bf03e19b8a9be9fcc9dd13f721a3f3b87cb4f7e6
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-03-22 05:46:46 +0300

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navigation">
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div>
+                    <a href="/order">Order</a>
+                    <a href="/review">Order Review</a>
+                    <a href="/inventory">Manage Inventory</a>
+                    <a href="/about">About</a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
