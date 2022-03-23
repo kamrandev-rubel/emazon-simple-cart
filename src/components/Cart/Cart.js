8dcd17eaeb6f45d2d755839129cc11c0665c3034
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-03-23 21:10:43 +0300

    set local storage id

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-    // let total = 0;
-    // let shipping = 0;
-    // for (const product of cart) {
-    //     total = total + product.price;
-    //     shipping = shipping + product.shipping
-    // }
+    /*
+     let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping
+    } 
+    */
     const total = cart.reduce((previous, current) => {
         return previous + current.price
     },0)
@@ -17,16 +19,14 @@ const Cart = ({ cart }) => {
     const totalPrice = total + shipping;
     const tax = (10 / 100) * totalPrice;
     const grandTotal = totalPrice + tax
-    // const taxTotal = Math.round(tax)
-    const taxTotal = tax.toFixed(2)
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
             <p>Total Price: ${ total }</p>
             <p>Total Shipping Charge: ${shipping}</p>
-            <p>Tax: ${taxTotal }</p>
-            <h3>Grand Total: ${ grandTotal}</h3>
+            <p>Tax: ${tax.toFixed(2) }</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2) }</h3>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,6 +15,7 @@ const Shop = () => {
     const addHandleCart = (product)=>{
         const newcart = [...cart, product]
         setCart(newcart)
+        addToDb(product.id)
     }
 return (
     <div className='shop-container'>
