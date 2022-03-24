ac9c8904beacc649c65deeee1b0dc93ea5984f88
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-03-24 17:01:38 +0300

    calculate quantity

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,27 +2,30 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-    /*
+    console.log('hello');
+    
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
-        shipping = shipping + product.shipping
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping * product.quantity
     } 
-    */
-    const total = cart.reduce((previous, current) => {
-        return previous + current.price
-    }, 0)
-    const shipping = cart.reduce((previous, current) => {
-        return previous + current.shipping
-    }, 0)
+   
+    // const total = cart.reduce((previous, current) => {
+    //     return previous + current.price
+    // }, 0)
+    // const shipping = cart.reduce((previous, current) => {
+    //     return previous + current.shipping
+    // }, 0)
     const totalPrice = total + shipping;
     const tax = (10 / 100) * totalPrice;
     const grandTotal = totalPrice + tax
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${ total }</p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: ${tax.toFixed(2) }</p>
