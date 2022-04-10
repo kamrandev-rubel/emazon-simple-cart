dbb26d32d41b57f882fad20f069f9a198017d6b1
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-04-10 05:00:59 +0300

    complete login components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
+import googleImage from '../../images/google.svg'
 
 const Login = () => {
     return (
         <div className='form-container'>
-            <div>
+            <div className='input-form-container'>
                 <h2 className='login-title'>Login</h2>
                 <form>
                     <div className="form-group">
@@ -21,6 +22,14 @@ const Login = () => {
                 <p>
                     New to Ema-john? <Link className='sign-up' to='/signup'>Create New Account</Link>
                 </p>
+                <div>
+                    <div className='login-form-style'>
+                        <div></div>
+                        <p>or</p>
+                        <div></div>
+                    </div>
+                    <button className='google-signin-btn'><img src={googleImage} alt="" /> Continue with Google</button>
+                </div>
             </div>
         </div>
     );
